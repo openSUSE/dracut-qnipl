65befeb545e83e21699c6c3992a16450f816055d
Nick Singer <nsinger@suse.de>
2018-03-22 10:29:26 +0100

    Add comments to the dracut module

--- module-setup.sh
+++ module-setup.sh
@@ -2,6 +2,7 @@
 
 # called by dracut
 check() {
+    # copied from 95qeth_rules. Maybe not needed at all
     local _arch=$(uname -m)
     local _online=0
     [ "$_arch" = "s390" -o "$_arch" = "s390x" ] || return 1
@@ -12,15 +13,26 @@ check() {
 
 # called by dracut
 install() {
+    # workaround for bsc#1086216
+    # required to setup network
     inst_simple /sbin/znetconf
+
+    # required runtime deps for znetconf
     inst_simple /usr/bin/basename
     inst_simple /usr/bin/awk
     inst_simple /lib/s390-tools/lsznet.raw
     inst_simple /lib/s390-tools/znetcontrolunits
     inst_simple /usr/bin/getopt
+
+    # required to list unconfigured interfaces (znetconf -u)
+    # maybe not needed for "znetconf -a" but for the sake of
+    # debugging we leave it in for now
     inst_simple /usr/bin/sort
     inst_simple /usr/bin/find
+
+    # needed for the cmdline parser of qnipl.sh
     inst_simple /usr/bin/cut
+
 
     inst_hook cmdline 99 "$moddir/qnipl.sh"
 }
